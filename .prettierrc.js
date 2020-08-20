3df01deea2f4532bd469356de5146a0e0fc0ef0a
Rahi <rahelehnazari@RahelehNazaris-MacBook-Pro.local>
2020-08-20 16:15:37 +0430

    add nextjs :sparkles:

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * Prettier configuration
+ * https://prettier.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>MyImgur</title>
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to myImgur
+        </h1>
+        <div className={styles.grid}>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div>footer</div>
+      </footer>
+    </div>
+  )
+}
